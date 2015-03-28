0a415ee0eb6ae5600af61643604209e48472ddea
Chris <chris.paterson86@gmail.com>
2015-03-28 18:07:09 +0000

    Test initial upload..

---
+++ RestaurantApp/Models/Dish.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantApp.Models
+{
+    public class Dish
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ RestaurantApp/Models/RestauRaterDB.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace RestaurantApp.Models
+{
+    public class RestauRaterDB : DbContext
+    {
+        public DbSet<Dish> Dishes { get; set; }
+    }
+}
